95619dc0f76e9514255767893f1acb578693792f
ChaeyoonWoo <heeni1004@gmail.com>
2024-05-29 15:00:34 +0900

    feat: 중간점검 merge
    feat: 중간점검 merge

--- app.js
+++ app.js
@@ -69,11 +69,11 @@ app.use("/mypage", mypageRouter);
 app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
-/*
+
 app.use(function (req, res, next) {
   next(createError(404));
 });
-*/
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
